63b158a6123a7df7b0ff2e1cfd1077804548c626
ZeroneDoo <rizqipambudi6@gmail.com>
2022-09-19 09:10:04 +0700

    sambung link css

--- tampil_siswa.php
+++ tampil_siswa.php
@@ -15,7 +15,7 @@ include "koneksi.php";
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
     <!-- link css -->
-    <link rel="stylesheet" href="tampil_siswa.css">
+    <link rel="stylesheet" href="css/tampil_siswa.css">
     <title>Tampil Data Siswa</title>
 </head>
 <body>
